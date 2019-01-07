3cc5cff0a251005a607af34e1d798f1165d64b7b
dilowagner <diegowagner4@gmail.com>
2019-01-07 21:33:09 -0200

    Replicate WaveState all Players

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -28,16 +28,22 @@ void ASGameMode::StartWave()
     NrOfBotsToSpawn = 2 * WaveCount;
 
     GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+
+    SetWaveState(EWaveState::WaveInProgress);
 }
 
 void ASGameMode::EndWave()
 {
     GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+    SetWaveState(EWaveState::WaitingToComplete);
 }
 
 void ASGameMode::PrepareForNextWave()
 {
     GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+
+    SetWaveState(EWaveState::WaitingToStart);
 }
 
 void ASGameMode::SpawnBotTimerElapsed()
@@ -83,6 +89,8 @@ void ASGameMode::CheckWaveState()
 
     if(! bIsAnyBotAlive)
     {
+        SetWaveState(EWaveState::WaveComplete);
+        
         PrepareForNextWave();
     }
 }
@@ -121,6 +129,8 @@ void ASGameMode::GameOver()
     EndWave();
 
     // @TODO: Finish up the match, present 'game over' to players
+
+    SetWaveState(EWaveState::GameOver);
 
     UE_LOG(LogTemp, Log, TEXT("GAME OVER! Players Died!"));
 }
